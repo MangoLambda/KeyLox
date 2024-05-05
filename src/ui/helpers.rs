15f79e72fdf55cd33f74c40fa71fbd74c0c1bb8e
MangoLambda <>
2024-05-05 16:29:58 -0400

    fixed bug where emails page with signle entry was displayed as empty

--- src/ui/helpers.rs
+++ src/ui/helpers.rs
@@ -3,5 +3,5 @@ use ratatui::widgets::Row;
 pub fn get_visible_rows(selected_index: usize, rows: Vec<Row>, height: u16) -> Vec<Row> {
     let first_item_in_row = selected_index - (selected_index % height as usize);
     let last_item_in_row = std::cmp::min(first_item_in_row + height as usize, rows.len() - 1);
-    rows[first_item_in_row..last_item_in_row].to_vec()
+    rows[first_item_in_row..=last_item_in_row].to_vec()
 }
