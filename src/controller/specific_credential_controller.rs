5cf2b004f43d9210811a96b6782947007f47e9ec
MangoLambda <>
2024-05-10 22:51:05 -0400

    fix saving credential

--- src/controller/specific_credential_controller.rs
+++ src/controller/specific_credential_controller.rs
@@ -5,36 +5,13 @@ use std::io;
 
 pub fn handle_specific_credential(app: &mut App, key_event: KeyEvent) -> Option<io::Result<bool>> {
     match key_event.code {
-        KeyCode::Enter | KeyCode::Tab | KeyCode::Down => {
-            app.cycle_editing_credential();
-        }
+        //TODO: Refactor to use key_events
         KeyCode::Up | KeyCode::BackTab => {
             app.reverse_cycle_editing_credential();
         }
         KeyCode::Esc => {
             app.current_screen = CurrentScreen::WebsiteCredentialScreen;
             app.discard_unsaved_credentials();
-        }
-        KeyCode::Char(value) => {
-            if let Some(editing) = &app.currently_editing_credential_field {
-                match editing {
-                    CurrentlyEditingCredentialField::Website => {
-                        app.website_input.push(value);
-                    }
-                    CurrentlyEditingCredentialField::Email => {
-                        app.email_input.push(value);
-                    }
-                    CurrentlyEditingCredentialField::Username => {
-                        app.username_input.push(value);
-                    }
-                    CurrentlyEditingCredentialField::Password => {
-                        app.password_input.push(value);
-                    }
-                    CurrentlyEditingCredentialField::Notes => {
-                        app.notes_input.push(value);
-                    }
-                }
-            }
         }
         KeyCode::Backspace => {
             if let Some(editing) = &app.currently_editing_credential_field {
@@ -69,6 +46,40 @@ pub fn handle_specific_credential(app: &mut App, key_event: KeyEvent) -> Option<
         } => {
             app.save_credential();
             app.current_screen = CurrentScreen::MainCredentialScreen;
+        }
+        KeyEvent {
+            code: KeyCode::Enter | KeyCode::Tab | KeyCode::Down,
+            modifiers: KeyModifiers::NONE,
+            kind: _,
+            state: _,
+        } => {
+            app.cycle_editing_credential();
+        }
+        KeyEvent {
+            code: KeyCode::Char(value),
+            modifiers: KeyModifiers::NONE,
+            kind: _,
+            state: _,
+        } => {
+            if let Some(editing) = &app.currently_editing_credential_field {
+                match editing {
+                    CurrentlyEditingCredentialField::Website => {
+                        app.website_input.push(value);
+                    }
+                    CurrentlyEditingCredentialField::Email => {
+                        app.email_input.push(value);
+                    }
+                    CurrentlyEditingCredentialField::Username => {
+                        app.username_input.push(value);
+                    }
+                    CurrentlyEditingCredentialField::Password => {
+                        app.password_input.push(value);
+                    }
+                    CurrentlyEditingCredentialField::Notes => {
+                        app.notes_input.push(value);
+                    }
+                }
+            }
         }
         _ => {}
     }
